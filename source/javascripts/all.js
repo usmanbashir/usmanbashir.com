69eb82175fc7b1a1f24201ae10ad4680a1841392
Usman Bashir <me@usmanbashir.com>
2015-01-14 02:39:52 +0000

    Add the sneak peek video for Currrva.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require jquery/dist/jquery
 //= require jquery-placeholder/jquery.placeholder.js
 //= require fastclick/lib/fastclick.js
+//= require foundation/js/foundation.js
 //= require_tree .
