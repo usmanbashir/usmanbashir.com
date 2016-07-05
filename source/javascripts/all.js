5e5cf2699a0e3d4196af3ebc3f4b1e0d26680c5b
Usman Bashir <me@usmanbashir.com>
2016-07-06 00:12:14 +0300

    Remove unnecessary JavaScript dependencies.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,4 @@
 //= require jquery/dist/jquery
-//= require jquery-placeholder/jquery.placeholder.js
 //= require fastclick/lib/fastclick.js
 //= require foundation/js/foundation.js
 //= require_tree .
